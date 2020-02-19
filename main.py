a21eb610d809f2b95d8c36dc77b151deb83df3ab
PrzemoNo1 <37351230+PrzemoNo1@users.noreply.github.com>
2020-02-19 21:02:05 +0100

    entry into arch

--- main.py
+++ main.py
@@ -6,14 +6,19 @@ wd = webdriver.Firefox()
 wd.get('https://www.nbp.pl/home.aspx?c=/ascx/archa.ascx')
 selects = wd.find_elements_by_xpath("//select")
 # rok_select.select_by_visible_text('2005')
+accept = wd.find_element_by_id('ctl25_btAscxSubmit')
 rok_select = selects[0]
 mies_select = selects[1]
 print(mies_select.text)
 
-time.sleep(5)
+time.sleep(1)
 
 year_select = Select(rok_select)
 year_select.select_by_visible_text('2005')
 
 month_select = Select(mies_select)
 month_select.select_by_visible_text(' maj ')
+
+time.sleep(1)
+
+accept.click()
